4864133be179f0cc57c0e80d2a76efd30d4a692c
Ramos <andfera201@gmail.com>
2025-02-01 19:52:19 -0500

    tests: The `tests.api.user_registration_mutation_test` tests were organized in a clearer way.

--- tests/api/user_registration_mutation_test.py
+++ tests/api/user_registration_mutation_test.py
@@ -41,14 +41,19 @@ async def test_successful_user_registration_with_valid_data(mock_prepare_db, cli
         'password': 'password_example',
     }
     response = client_api.post('/graphql', json={'query': mutation_register_user, 'variables': mutation_variables})
-    response_data = response.json()
+    response_json = response.json()
 
-    result_register_user = response_data['registerUser']
-    assert result_register_user['ok'] is True
-    assert result_register_user['message'] == 'User registered successfully.'
-    assert result_register_user['user']['id'] is not None
+    assert response_json == {
+        'registerUser': {
+            'ok': True,
+            'message': 'User registered successfully.',
+            'user': {
+                'id': '1',
+            },
+        },
+    }
 
-    user_created = await User.get(id=result_register_user['user']['id'])
+    user_created = await User.get(id=response_json['registerUser']['user']['id'])
     assert user_created.name == mutation_variables['name']
     assert user_created.last_name == mutation_variables['lastName']
     assert user_created.username == mutation_variables['username']
@@ -71,15 +76,16 @@ async def test_error_when_user_already_exists_in_user_registration(
         'username': existing_user.username,
         'email': existing_user.email,
         'mobilePhone': existing_user.mobile_phone,
-        'password': existing_user. password,
+        'password': existing_user.password,
     }
     response = client_api.post('/graphql', json={'query': mutation_register_user, 'variables': mutation_variables})
-    response_data = response.json()
-
-    result_register_user = response_data['registerUser']
-    assert result_register_user['ok'] is False
-    assert result_register_user['message'] == 'Error: mobile_phone data already exists'
-    assert result_register_user['user'] is None
+    assert response.json() == {
+        'registerUser': {
+            'ok': False,
+            'message': 'Error: mobile_phone data already exists',
+            'user': None,
+        },
+    }
 
 
 @mark.asyncio
@@ -93,12 +99,13 @@ async def test_error_when_fields_are_empty_strings(mock_prepare_db, client_api):
         'password': '',
     }
     response = client_api.post('/graphql', json={'query': mutation_register_user, 'variables': mutation_variables})
-    response_data = response.json()
-
-    result_register_user = response_data['registerUser']
-    assert result_register_user['ok'] is False
-    assert result_register_user['message'] == 'The field "name" cannot be empty or contain only spaces.'
-    assert result_register_user['user'] is None
+    assert response.json() == {
+        'registerUser': {
+            'ok': False,
+            'message': 'The field "name" cannot be empty or contain only spaces.',
+            'user': None,
+        },
+    }
 
 
 @mark.asyncio
@@ -112,14 +119,10 @@ async def test_error_when_required_fields_are_none(client_api):
         'password': None,
     }
     response = client_api.post('/graphql', json={'query': mutation_register_user, 'variables': mutation_variables})
-    response_data = response.json()
-
-    assert response_data == {'error': 'There was a problem with the fields provided. Please check the inputs.'}
+    assert response.json() == {'error': 'There was a problem with the fields provided. Please check the inputs.'}
 
 
 @mark.asyncio
 async def test_error_when_variables_are_none(client_api):
     response = client_api.post('/graphql', json={'query': mutation_register_user, 'variables': None})
-    response_data = response.json()
-
-    assert response_data == {'error': 'There was a problem with the fields provided. Please check the inputs.'}
+    assert response.json() == {'error': 'There was a problem with the fields provided. Please check the inputs.'}
